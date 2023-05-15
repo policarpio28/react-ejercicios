210edc0d04decc38227a9f18d50795e5ab34fae2
marcosgit <marcosgit@gmail.com>
2023-05-15 13:46:23 +0200

    Clase 29. CRUD App - finalizada

--- src/components/28.CrudApp.js
+++ src/components/28.CrudApp.js
@@ -35,13 +35,31 @@ const CrudApp = () => {
   // esto es lo q nos traeria de bbdd q es un json
   const [db, setDb] = useState(inicialDb);
 
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  // añade un registro al array y repinta el render
+  const createData = (data) => {
+    // creamos un id
+    data.id = Date.now();
+    // lo que ya tenga el objeto , objeto a añadir
+    setDb([...db, data]);
+  };
+  const updateData = (data) => {};
+  const delateData = (id) => {};
+
   return (
     <>
       <h2>CRUD App con datos locales (Sin apis)</h2>
       {/*formulario para añadir mas objetos*/}
-      <CrudForm />
+      <CrudForm
+        //en vez de crear los metodos en el form se los pasamos desde aqui para probar q se puede hacer
+        createData={createData}
+        updateData={updateData}
+        dataToEdit={dataToEdit}
+        setDataToEdit={setDataToEdit}
+      />
       {/*tabla donde mostrar todo pasando los objetos*/}
-      <CrudTable data={db} />
+      <CrudTable data={db} updateData={updateData} delateData={delateData} />
     </>
   );
 };
--- src/components/28.CrudForm.js
+++ src/components/28.CrudForm.js
@@ -7,15 +7,45 @@ const initiacialForm = {
   constellation: "",
 };
 
-const CrudForm = () => {
+//desestructruramos las propiedades pasadas desde el CrudApp props.createDate  ....
+const CrudForm = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
   {
     /*constante que engloba a los input*/
   }
   const [form, setForm] = useState(initiacialForm);
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
-  const handleReset = (e) => {};
+  const handleChange = (e) => {
+    setForm({
+      // lo que ya tenga el objeto , nombre del objeto : valor del objeto
+      // en este caso puede ser form, constellation : lo_que_escribas
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault(); // para no autoprocesar el formulario
+
+    //validacion
+    if (!form.name || !form.constellation) {
+      alert("Datos incompletos");
+      return;
+    }
+
+    // si no tiene id es nuevo sino es actualizacion
+    if (form.id === null) {
+      createData(form);
+    } else {
+      updateData(form);
+    }
+
+    handleReset();
+  };
+
+  //limpia los input
+  const handleReset = (e) => {
+    setForm(initiacialForm);
+    setDataToEdit(null);
+  };
 
   return (
     <div>
