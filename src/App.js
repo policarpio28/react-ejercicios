03ea424a927405ed63ceb313b817b67bc8a7957b
marcosgit <marcosgit@gmail.com>
2023-05-16 14:32:12 +0200

    Clase 32. CRUD API - finalizada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CrudApp from "./components/28_31.CrudApp";
+import CrudApi from "./components/32.CrudApi";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
-      <CrudApp />
+      <CrudApi />
       <hr />
+      <CrudApp />
     </>
   );
 }
---
+++ src/components/32.CrudApi.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import CrudForm from "./28_31.CrudForm";
+import CrudTable from "./28_31.CrudTable";
+
+const CrudApi = () => {
+  // esto es lo q nos traeria de bbdd q es un json
+  const [db, setDb] = useState([]);
+
+  // elemento que se va a editar
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  // añade un registro al array y repinta el render
+  const createData = (data) => {
+    // creamos un id
+    data.id = Date.now();
+    // lo que ya tenga el objeto , objeto a añadir
+    // repinta el render
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    // comparamos el elemento, si es el mismo id reemplzamos el elemento por
+    //el data que es modificado sino dejamos el mismo
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    // añadimos la nueva lista |  repinta el render
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `Estas seguro de eliminar el registro con id '${id}' ??`
+    );
+
+    if (isDelete) {
+      // soltra por los elemento que no es el id pasado para nuevo añadirlos a la setData
+      //en vez de hacer un delete del propio elemento
+      let newData = db.filter((el) => el.id !== id);
+      // añadimos la nueva lista |  repinta el render
+      setDb(newData);
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <>
+      <h2>CRUD App con datos locales (Sin apis)</h2>
+      <article className="grid-1-2">
+        {/*formulario para añadir mas objetos*/}
+        <CrudForm
+          //en vez de crear los metodos en el form se los pasamos desde aqui para probar q se puede hacer
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        {/*tabla donde mostrar todo pasando los objetos*/}
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </>
+  );
+};
+
+export default CrudApi;
