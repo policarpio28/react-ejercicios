1f5c540bb5bc85a9cbd3006fe69ac03ba292ee8c
marcosgit <marcosgit@gmail.com>
2023-05-28 12:54:33 +0200

    Clase 39. Buscador de Canciones - finalizada

--- src/components/37.SongSearch.js
+++ src/components/37.SongSearch.js
@@ -2,12 +2,43 @@ import React, { useEffect, useState } from "react";
 import SongForm from "./37.SongForm";
 import SongDetails from "./37.SongDetails";
 import Loader from "./32_36.Loader";
+import { helpHttp } from "../helpers/32_36.helpHttp";
 
 const SongSearch = () => {
   const [search, setSearch] = useState(null);
   const [cancion, setCancion] = useState(null);
   const [biografia, setBiografia] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //para no renderizar innecesariamente
+    if (search === null) {
+      return;
+    }
+
+    const fetchData = async () => {
+      const { artist, cancion } = search;
+
+      let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+      let cancionUrl = `https://api.lyrics.ovh/v1/${artist}/${cancion}`;
+
+      setLoading(true);
+
+      // con all hace todas las peticiones y lo guarda la respuesta en cada una
+      const [artistRes, cancionRes] = await Promise.all([
+        helpHttp().get(artistUrl),
+        helpHttp().get(cancionUrl),
+      ]);
+
+      //setea los valores de la respuesta
+      setBiografia(artistRes);
+      setCancion(cancionRes);
+
+      setLoading(false);
+    };
+    // cuando es asincrona react recomienda ponerlo asi por separado
+    fetchData();
+  }, [search]);
 
   const handleSearch = (data) => {
     setSearch(data);
