88017410b8d6f31a1a9bc290ba910570a0502e29
marcosgit <marcosgit@gmail.com>
2023-06-01 19:52:32 +0200

    Clase 42. Selects Anidados - finalizada

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import CrudApp from "./components/28_31.CrudApp";
 import CrudApi from "./components/32_36.CrudApi";
 import SongSearch from "./components/37_41.SongSearch";
+import SelectsAnidados from "./components/42.SelectsAnidados";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <SelectsAnidados />
+      <hr />
       <SongSearch />
       <hr />
       <CrudApi />
