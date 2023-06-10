4ce9745bc7773a6c6e81744ed3cc0a8d2c6299d2
marcosgit <marcosgit@gmail.com>
2023-06-10 13:19:44 +0200

    Clase 52: Portales  - finalizada

--- src/components/49_51-52.Modals.js
+++ src/components/49_51-52.Modals.js
@@ -2,12 +2,16 @@ import React from "react";
 import Modal from "./49_51.Modal";
 import { use45_48Modal } from "../hooks/use49_51Modal";
 import ContactForm from "./45_48.ContactForm";
+import ModalPortal from "./52.ModalPortal";
 
 const Modals = () => {
   // se hacen aqui las invocaciones y no en 49_51.Modal.js para controlar cada modal por seraparado
   const [isOpenModal1, openModal1, closeModal1] = use45_48Modal(false);
   const [isOpenModal2, openModal2, closeModal2] = use45_48Modal(false);
   const [isOpenModalContact, openModalContact, closeModalContact] =
+    use45_48Modal(false);
+
+  const [isOpenModalPortal, openModalPortal, closeModalPortal] =
     use45_48Modal(false);
 
   return (
@@ -31,6 +35,17 @@ const Modals = () => {
       <Modal isOpen={isOpenModalContact} closeModal={closeModalContact}>
         <ContactForm />
       </Modal>
+
+      {/*Modal portal de la clase 52 , se ha tocado el index.html para esto añadiendo un div */}
+      <button onClick={openModalPortal}>Modal Portal</button>
+      <ModalPortal isOpen={isOpenModalPortal} closeModal={closeModalPortal}>
+        <h3>Modal Portal</h3>
+        <p>
+          Este es el contenido de un modal que carga en otro nodo DOM difrente a
+          donde carga nuestra app de react, gracias a un react portal.
+        </p>
+        <img src="https://placeimg.com/400/400/tech" alt="Tech" />
+      </ModalPortal>
     </div>
   );
 };
---
+++ src/components/52.ModalPortal.js
@@ -0,0 +1,29 @@
+import "./49_51.Modal.css";
+
+import ReactDOM from "react-dom";
+
+// props.children es una palabra reservada que recibe desde la ventana padre
+// que son las cosas que hay entre <Modal>...</Modal>
+const ModalPortal = ({ children, isOpen, closeModal }) => {
+  //esto hace que cuando pulsamos dentro del modal no se cierre solo por cupa el onclick del article
+  const hadleModalContainerClick = (e) => e.stopPropagation();
+
+  // creacion de portal
+  return ReactDOM.createPortal(
+    //con la clase style "modal" solo no se veria pero si le llega isOpen añade el is-open que es el que lo muestra
+    // añadimos el onClick para poder tambien dar fuera del modal y cerrarlo pero ponemos hadleModalContainerClick
+    // para que no se cieere el modal pulsar dentro del él
+    <article className={`modal ${isOpen && "is-open"}`} onClick={closeModal}>
+      <div className="modal-container" onClick={hadleModalContainerClick}>
+        <button className="modal-close" onClick={closeModal}>
+          X
+        </button>
+        {children}
+      </div>
+    </article>,
+    // asignadcion de el modal a la etiqueta de hay en el index.html creada
+    document.getElementById("modal")
+  );
+};
+
+export default ModalPortal;
